1907e502b03f98796929c6fc6df8ea6c758ae34f
Lucas <lucasvalenteds@gmail.com>
2020-11-13 23:01:37 -0300

    Extracting configuration to properties file

--- src/main/java/com/example/spring/AppConfiguration.java
+++ src/main/java/com/example/spring/AppConfiguration.java
@@ -4,8 +4,13 @@ import com.example.spring.person.PersonHandler;
 import com.example.spring.person.PersonRepository;
 import com.example.spring.person.PersonRouter;
 import com.mongodb.ConnectionString;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.GenericApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.StandardEnvironment;
+import org.springframework.core.io.support.ResourcePropertySource;
 import org.springframework.data.mongodb.core.ReactiveFluentMongoOperations;
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
 import org.springframework.data.mongodb.core.SimpleReactiveMongoDatabaseFactory;
@@ -15,17 +20,37 @@ import org.springframework.web.server.WebHandler;
 import org.springframework.web.server.adapter.WebHttpHandlerBuilder;
 import reactor.netty.http.server.HttpServer;
 
+import java.io.IOException;
+
 public final class AppConfiguration {
+
+    private static final Logger LOGGER = LogManager.getLogger(AppConfiguration.class);
 
     private AppConfiguration() {
     }
 
-    public static ApplicationContext createApplicationContext() {
+    private static class PropertyBasedEnvironment extends StandardEnvironment {
+
+        public PropertyBasedEnvironment(String filename) {
+            try {
+                getPropertySources().addLast(new ResourcePropertySource(filename));
+            } catch (IOException exception) {
+                LOGGER.warn("Could not read the file {}.", filename, exception);
+            }
+        }
+    }
+
+    public static ConfigurableEnvironment environment() {
+        return new PropertyBasedEnvironment("classpath:/application.properties");
+    }
+
+    public static ApplicationContext createApplicationContext(ConfigurableEnvironment environment) {
         GenericApplicationContext context = new GenericApplicationContext();
+        context.setEnvironment(environment);
 
         context.registerBean(ReactiveFluentMongoOperations.class, () ->
             new ReactiveMongoTemplate(new SimpleReactiveMongoDatabaseFactory(new ConnectionString(
-                "mongodb://localhost:27017/person"
+                environment.getRequiredProperty("database.url", String.class)
             )))
         );
         context.registerBean(WebHttpHandlerBuilder.WEB_HANDLER_BEAN_NAME, WebHandler.class, () ->
@@ -43,7 +68,7 @@ public final class AppConfiguration {
 
     public static HttpServer createHttpServer(ApplicationContext context) {
         return HttpServer.create()
-            .port(8080)
+            .port(context.getEnvironment().getRequiredProperty("server.port", Integer.class))
             .handle(new ReactorHttpHandlerAdapter(WebHttpHandlerBuilder.applicationContext(context).build()));
     }
 }
--- src/main/java/com/example/spring/Main.java
+++ src/main/java/com/example/spring/Main.java
@@ -3,6 +3,7 @@ package com.example.spring;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.context.ApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
 import reactor.netty.http.server.HttpServer;
 
 import java.time.Duration;
@@ -12,7 +13,8 @@ public class Main {
     private static final Logger LOGGER = LogManager.getLogger(Main.class);
 
     public static void main(String[] args) {
-        ApplicationContext context = AppConfiguration.createApplicationContext();
+        ConfigurableEnvironment environment = AppConfiguration.environment();
+        ApplicationContext context = AppConfiguration.createApplicationContext(environment);
         HttpServer httpServer = AppConfiguration.createHttpServer(context);
 
         httpServer.bindUntilJavaShutdown(
