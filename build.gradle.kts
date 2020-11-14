d87ca97f16fda730ed7613d5df172b7fc0e6b4ae
Lucas <lucasvalenteds@gmail.com>
2020-11-13 22:41:41 -0300

    Adding Gradle build script

---
+++ build.gradle.kts
@@ -0,0 +1,45 @@
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+
+plugins {
+    java
+    application
+}
+
+repositories {
+    jcenter()
+}
+
+dependencies {
+    implementation("org.apache.logging.log4j", "log4j-api", properties["version.log4j"].toString())
+    implementation("org.apache.logging.log4j", "log4j-core", properties["version.log4j"].toString())
+    implementation("org.slf4j", "slf4j-simple", properties["version.slf4j"].toString())
+
+    implementation("org.springframework", "spring-webflux", properties["version.spring.web"].toString())
+    implementation("org.springframework", "spring-context", properties["version.spring.web"].toString()) {
+        exclude(module = "spring-aop")
+    }
+    testImplementation("org.springframework", "spring-test", properties["version.spring.web"].toString())
+
+    implementation("io.projectreactor", "reactor-core", properties["version.reactor"].toString())
+    testImplementation("io.projectreactor", "reactor-test", properties["version.reactor"].toString())
+    implementation("io.projectreactor.netty", "reactor-netty", properties["version.netty"].toString())
+
+    implementation("org.springframework.data", "spring-data-mongodb", properties["version.spring.data"].toString())
+    implementation("org.mongodb", "mongodb-driver-reactivestreams", properties["version.mongodb"].toString())
+    testImplementation("de.flapdoodle.embed", "de.flapdoodle.embed.mongo", properties["version.mongodb.embed"].toString())
+
+    implementation("com.fasterxml.jackson.core", "jackson-databind", properties["version.jackson"].toString())
+
+    testImplementation("org.junit.jupiter", "junit-jupiter", properties["version.junit"].toString())
+}
+
+configure<ApplicationPluginConvention> {
+    mainClassName = "com.example.spring.Main"
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+    testLogging {
+        events = setOf(TestLogEvent.PASSED, TestLogEvent.FAILED, TestLogEvent.SKIPPED)
+    }
+}
